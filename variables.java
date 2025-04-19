11aa165c051941b42ee9acea720fb1bddc0bbf78
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-18 21:01:45 -0400

    Initial commit

---
+++ variables.java
@@ -0,0 +1,11 @@
+class variables {
+    public static void main(String a[]) {
+        int number1 = 4;
+        int number2 = 6;
+        int result = number1 + number2;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(result);
+    }
+
+}
