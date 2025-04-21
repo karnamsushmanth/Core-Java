f6dabd192a20e04c3cb808c128c7290f83ac1edd
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-21 13:40:12 -0400

    Variables

---
+++ Datatypes.java
@@ -0,0 +1,31 @@
+// data types - primitive , 
+// primitive
+// Int -2, ....,2  [byte -- 1 bytes , short -- 2 bytes, int -- 4 bytes  , long -- 8 bytes]
+// Float - 0.567  [float and double]
+// char - ['s'.'p'] - 2 bytes
+// boolean->  [true and false]
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int num1 = 9;
+
+        float marks = 6.5f;
+        byte by = 127;
+        short sh = 558;
+        long l = 558885l;
+        float f = 5.8f;
+        double d = 5.8;
+        char c = 's';
+        boolean b = true;
+        System.out.println(marks);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+
+    }
+
+}
