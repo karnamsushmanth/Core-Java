67050208f68e881589122e17ce3618faf9113bcc
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-21 16:35:41 -0400

    Add Literals.java

---
+++ Literals.java
@@ -0,0 +1,14 @@
+public class Literals {
+    public static void main(String[] args) {
+        char c = 'a';
+        System.out.println(c);
+        c++;
+        System.out.println(c);
+        int num1 = 0b101;
+        System.out.println(num1);
+        double d1 = 12e10;
+        System.out.println(d1);
+
+    }
+
+}
