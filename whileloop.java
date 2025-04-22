eae4138c8f6793450939ddf8fa776840ec9f4a2b
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-22 16:48:57 -0400

    while loop

---
+++ whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+    public static void main(String a[]) {
+        int i = 1;
+        while (i < 10) {
+            System.out.println("Hi" + " " + i);
+            i++;
+        }
+        System.out.println("Bye" + " " + i);
+
+    }
+
+}
