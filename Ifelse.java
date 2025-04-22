a69b943538a4bbb4baf7af250cbe7361a0a120e6
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-22 15:58:08 -0400

    if else statements

---
+++ Ifelse.java
@@ -0,0 +1,20 @@
+public class Ifelse {
+    public static void main(String[] args) {
+        int x = 18;
+        if (x > 10 && x <= 20) {
+            System.out.println("hello");
+        } else {
+            System.out.println("bye");
+        }
+        // greatest of two numbers
+
+        int num1 = 6;
+        int num2 = 7;
+        if (num1 > num2) {
+            System.out.println("number 1 is greater");
+        } else {
+            System.out.println("number 2 is greater");
+        }
+    }
+
+}
