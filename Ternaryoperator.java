06224a530547b23a2bd49e7f7fa2ff49a27f1776
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-22 16:14:04 -0400

    ternary opertors

---
+++ Ternaryoperator.java
@@ -0,0 +1,15 @@
+public class Ternaryoperator {
+    public static void main(String[] args) {
+        // even or odd
+        int n = 4;
+        // int result = 0;
+
+        // if (n % 2 == 0) {
+        // System.out.println("even");
+        // } else {
+        // System.out.println("odd");
+        // }
+        int result = n % 2 == 0 ? 10 : 20;
+        System.out.println(result);
+    }
+}
