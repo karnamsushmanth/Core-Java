232964fa821ddfc0d67a78fa9558b6fa8e5e50b4
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-22 16:54:05 -0400

    do while

---
+++ dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile {
+    public static void main(String[] args) {
+
+        int i = 6;
+        do {
+            System.out.println("hello");
+            i++;
+        } while (i > 10);
+    }
+
+}
