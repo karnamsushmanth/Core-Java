4dab6114d9b7463218a8365858fc0b12518dee49
karnamsushmanth <karnamsushmanth@gmail.com>
2025-04-22 15:25:43 -0400

    relational  operators

---
+++ Relation.java
@@ -0,0 +1,18 @@
+public class Relation {
+    public static void main(String a[]) {
+        // <, > ,<= ,>= ,==, !=
+        // T --> F
+
+        int x = 6;
+        int y = 5;
+        boolean result = x < y;
+        boolean result1 = x > y;
+        boolean equal = x == y;
+        boolean notequal = x != y;
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(equal);
+        System.out.println(notequal);
+    }
+
+}
